17aad3ccd70cf1f6a5449a729a64c68b9cdd6678
TEBL <teblunde@gmail.com>
2025-10-22 02:49:11 +0200

    Create package under release

--- install.py
+++ install.py
@@ -9,6 +9,8 @@ import os
 from pathlib import Path
 
 DESKLETS_PATH = f'{Path.home()}/.local/share/cinnamon/desklets/'
+RELEASE_PATH = "release"
+MESSAGE_INDENT = 22
 
 def copy_xlet(uuid):
     """
@@ -20,7 +22,7 @@ def copy_xlet(uuid):
     try:
         shutil.copytree(uuid_path, destination, dirs_exist_ok=True)
         reload_xlet(f'{uuid}')
-        print_info("Desklet installed OK", destination)
+        print_keypair("Desklet installed OK", destination)
     except (FileNotFoundError, KeyError):
         # metadata.json file not found or missing valid keys
         pass
@@ -37,22 +39,28 @@ def reload_xlet(uuid):
         print(out.stderr.decode('ascii') + '\nReload error!')
 
 
-def package_xlet(uuid):
+def package_xlet(uuid, force):
     uuid_path = f'{uuid}/files/{uuid}'
     zip_path = get_package_path(uuid, uuid_path)
-    with zipfile.ZipFile(zip_path, 'w') as zip_file:
-        for root, dirs, files in os.walk(uuid_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                arcname = os.path.relpath(file_path, start=os.path.dirname(uuid_path))
-                zip_file.write(file_path, arcname, compress_type=zipfile.ZIP_DEFLATED)
-    print_info("Package created OK", zip_path)
+    if os.path.isfile(zip_path) == False or force:
+        with zipfile.ZipFile(zip_path, 'w') as zip_file:
+            for root, dirs, files in os.walk(uuid_path):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    arcname = os.path.relpath(file_path, start=os.path.dirname(uuid_path))
+                    zip_file.write(file_path, arcname, compress_type=zipfile.ZIP_DEFLATED)
+        print_keypair("Package created OK", zip_path)
+    else:
+        print_error("Package ERROR", zip_path, "package already exists")
 
 
 def get_package_path(uuid, uuid_path):
+    if not os.path.isdir(RELEASE_PATH):
+        os.makedirs(RELEASE_PATH)
+
     shortname = uuid.split('@')[0]
     version = get_version(uuid_path)
-    return f'{shortname}-{version}.zip'
+    return os.path.join(RELEASE_PATH, f'{shortname}-{version}.zip')
 
 
 def get_version(uuid_path):
@@ -74,15 +82,18 @@ def get_uuid():
                 uuid_metadata = f'{uuid}/files/{uuid}/metadata.json'
 
                 if os.path.isfile(uuid_metadata):
-                    print_info("UUID detected", uuid)
+                    print_keypair("UUID detected", uuid)
                     return uuid
         break
     raise Exception('Unable to get UUID')
 
 
-def print_info(key, value):
-    print((key + ":").ljust(22, ' '), value)
+def print_keypair(key, value):
+    print((key + ":").ljust(MESSAGE_INDENT, ' '), value)
 
+def print_error(key, value, reason):
+    print((key + ":").ljust(MESSAGE_INDENT, ' '), value)
+    print(" ".rjust(MESSAGE_INDENT, ' '), "(" + reason + ")")
 
 def main():
     """
@@ -93,12 +104,13 @@ def main():
     parser.description = 'Desklet installation and packaging'
     parser.add_argument('-i', '--install', action='store_true', help='Install desklet for current user')
     parser.add_argument('-z', '--zip', action='store_true', help='Package desklet for release')
+    parser.add_argument('--force', action='store_true', help='Used with --zip to allow overwriting release file')
     args = parser.parse_args()
 
     if args.install:
         copy_xlet(get_uuid())
     elif args.zip:
-        package_xlet(get_uuid())
+        package_xlet(get_uuid(), args.force)
     else:
         parser.print_help()
 
